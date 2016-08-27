bce13f8d05b557b07d9cd1989faaba0dd84f211e
yasiasz <yurizapata22@gmail.com>
2016-08-26 20:13:25 -0500

    firsr

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+    
+  devise_for :users
+  root to: 'static_pages#bienvenida'
+
+	post '/contact', to: 'static_pages#create'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
